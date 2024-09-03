813f64234c6e6abffa7c5f023279809d4e60522e
Alex <aw729322@ohio.edu>
2024-09-03 17:36:49 -0400

    All Done!

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alex Wagner
+ *        Date: 9/3/24
+ * Description: git lab project
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+  cout << "Hello Git!!" << endl;
+    return 0;
+}// main
